7e69a7c90bb7ee062897bf41a7c470997df8c31a
Adam Kagdi <71578844+adamkagdi@users.noreply.github.com>
2024-03-07 14:11:46 +0400

    few changes

---
+++ relational_operations/src/main/java/com/relation_operations/Check.java
@@ -0,0 +1,29 @@
+package com.relation_operations;
+
+import java.util.Scanner;
+
+class Relational{
+    double a;
+    double b;
+}
+public class Check {
+    public static void main(String[] args) {
+        Relational r1 = new Relational();
+        System.out.println("Please enter number a: ");
+        Scanner s = new Scanner(System.in);
+        System.out.println("Please enter number b: ");
+        Scanner f = new Scanner(System.in);
+        r1.a = s.nextDouble();
+        r1.b = f.nextDouble();
+        System.out.println(r1.a == r1.b);
+        System.out.println(r1.a != r1.b);
+        System.out.println(r1.a > r1.b);
+        System.out.println(r1.a < r1.b);
+        System.out.println(r1.a >= r1.b);
+        System.out.println(r1.a <= r1.b);
+
+        System.out.println(r1.a > r1.b && r1.a > 8);
+        System.out.println(r1.b > r1.a || r1.b > 7);
+        System.out.println(!(r1.a == r1.b));
+    }
+}
