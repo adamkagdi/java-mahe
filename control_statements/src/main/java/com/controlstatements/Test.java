d592aa5f56f638f784c4145da386f60d2a0d6730
Adam Kagdi <71578844+adamkagdi@users.noreply.github.com>
2024-03-13 11:28:41 +0400

    new class update

---
+++ control_statements/src/main/java/com/controlstatements/Test.java
@@ -0,0 +1,16 @@
+package com.controlstatements;
+import java.util.Scanner;
+
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner s1 = new Scanner(System.in);
+        System.out.println("Enter any first number: ");
+        double x = s1.nextDouble();
+        System.out.println("Enter any second number: ");
+        double y = s1.nextDouble();
+        if (x > y){
+            System.out.println("x is the greatest");
+        }
+    }
+}
